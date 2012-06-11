03245af069457b33d904110595f0aaa82bca2f11
JD Bothma <jbothma@gmail.com>
2012-06-11 18:08:35 +0200

    Fix memory leak when looping over documents with Factory.deleteResource(...)

--- src/uk/co/jbothma/relations/RelCandidateExtractor.java
+++ src/uk/co/jbothma/relations/RelCandidateExtractor.java
@@ -2,6 +2,7 @@ package uk.co.jbothma.relations;
 
 import gate.Annotation;
 import gate.AnnotationSet;
+import gate.Corpus;
 import gate.DataStore;
 import gate.Document;
 import gate.Factory;
@@ -15,11 +16,13 @@ import gate.util.GateException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class RelCandidateExtractor {
-	private static final String dataStorePath = "/home/jdb/thesis/results/jrc2006_korp_mid";
+	private static final String dataStorePath = "/home/jdb/thesis/results/jrc2006_korp_big";
 	private static final String outfilePath = "/home/jdb/thesis/results/Relations.txt";
 
 	/**
@@ -34,36 +37,34 @@ public class RelCandidateExtractor {
 		AnnotationSet inputAS;
 		String inputASName, relationASType;
 		gate.Document doc;
-		String npWordsPos, pos, word, phrase;
 		Annotation relationAnnot;
-		AnnotationSet tokAnnots;
-		List<Annotation> tokAnnotList;
 		FileWriter fstream = new FileWriter(outfilePath);
-		BufferedWriter out = new BufferedWriter(fstream);
-
+		BufferedWriter out = new BufferedWriter(fstream, 20);
+		
+		printMem();
 		Gate.init();
+		printMem();
 		
 		// get the datastore
 		dataStore = (SerialDataStore) Factory.openDataStore(
 				"gate.persist.SerialDataStore", "file:///" + dataStorePath);
 		dataStore.open();
 		
-		int totalDocs = dataStore.getLrIds("gate.corpora.DocumentImpl").size();
-		int docIdx = 0;
+		printMem();
+		
+		FeatureMap corpFeatures = Factory.newFeatureMap();
+		corpFeatures.put(DataStore.LR_ID_FEATURE_NAME, dataStore.getLrIds("gate.corpora.SerialCorpusImpl").get(0));
+		corpFeatures.put(DataStore.DATASTORE_FEATURE_NAME, dataStore);
+		Corpus persistCorp = (Corpus)Factory.createResource("gate.corpora.SerialCorpusImpl", corpFeatures);
+		
+		printMem();
 		
 		// get the corpus
-		for (Object docID : dataStore.getLrIds("gate.corpora.DocumentImpl")) {
-			docFeatures = Factory.newFeatureMap();
-			docFeatures.put(DataStore.LR_ID_FEATURE_NAME, docID);
-			docFeatures.put(DataStore.DATASTORE_FEATURE_NAME, dataStore);
-			
-			//tell the factory to load the Serial Corpus with the specified ID from the specified  datastore
-			doc = (gate.Document)
-					Factory.createResource("gate.corpora.DocumentImpl", docFeatures);
-			
-
-			docIdx++;
-			System.out.println("Doc " + docIdx + " / " + totalDocs + " : " + doc.getName());
+		for (int docIdx = 0; docIdx < persistCorp.size(); docIdx++) {
+			printMem();
+			doc = persistCorp.get(docIdx);
+			printMem();
+			System.out.println("Doc " + (docIdx+1) + " / " + persistCorp.size() + " : " + doc.getName());
 			
 			inputASName = "Original markups";
 			inputAS = doc.getAnnotations(inputASName);
@@ -73,20 +74,45 @@ public class RelCandidateExtractor {
 			subclassIter = inputAS.get("Domain").iterator();
 			
 			while (relationIter.hasNext()) {
-				phrase = "";
 				relationAnnot = (Annotation) relationIter.next();
 				String superclass = superclass(inputAS, relationAnnot);
-				System.out.println(superclass + " såsom");
-				//out.write(phrase + "\n");
+				Set<String> subclasses = subclasses(inputAS, relationAnnot);
+				for (String subclass : subclasses) {
+					System.out.println(superclass + " <--- " + subclass);
+					out.write(superclass + " <--- " + subclass + "\n");
+				}
 			}
+			
+			persistCorp.unloadDocument(doc);
+			Factory.deleteResource(doc);
 		}
-		
 		out.close();
 	}
 	
+	private static void printMem() {
+		Runtime runtime = Runtime.getRuntime();
+		int mb = 1024*1024;
+		System.out.println("Used Memory:"
+	            + (runtime.totalMemory() - runtime.freeMemory()) / mb
+	            + " / " + runtime.maxMemory() / mb);
+	}
+
 	private static String superclass(AnnotationSet inputAS, Annotation relation) {
 		AnnotationSet superclassAS = inputAS.get("Range");
 		AnnotationSet containedSuperclassAS = Utils.getContainedAnnotations(superclassAS, relation);
 		return Utils.stringFor(inputAS.getDocument(), containedSuperclassAS);
+	}
+
+	private static Set<String> subclasses(AnnotationSet inputAS, Annotation relation) {
+		AnnotationSet subclassAS = inputAS.get("Domain");
+		AnnotationSet termAS = inputAS.get("TermCandidate");
+		AnnotationSet containedSubclassPartAS = Utils.getContainedAnnotations(subclassAS, relation);
+		AnnotationSet subclassTermsAS = Utils.getContainedAnnotations(termAS, containedSubclassPartAS);
+		Set<String> subclassStrings = new HashSet<String>();
+		Iterator<Annotation> iter = subclassTermsAS.iterator();
+		while (iter.hasNext()) {
+			subclassStrings.add(Utils.stringFor(inputAS.getDocument(), iter.next()));
+		}
+		return subclassStrings;
 	}
 }
